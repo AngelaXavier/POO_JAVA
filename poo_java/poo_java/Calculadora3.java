843b9ff34e00c11591f5a262c5312b6a84db9658
Angela Xavier <angelaxfeliciano@gmail.com>
2026-02-27 20:58:48 -0300

    Update Calculadora3.java

--- poo_java/poo_java/Calculadora3.java
+++ poo_java/poo_java/Calculadora3.java
@@ -4,7 +4,7 @@ public class Clculadora2 {
   public static void main (String) arg {
       scanner scanner = new Scanner(System.in);
         System.out.print("Digite o primeiro número: ");
-    doubl numero1 = scanner.nextDouble();
+    double numero1 = scanner.nextDouble();
     System.out.print("Digite o segundo número: ");
         double numero2 = scanner.nextDouble();
     System out.print("Digite a operação (+, -, *, /): ")
